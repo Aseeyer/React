c0550d1fefaea4b0e52328fad8e729ee8722b89c
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-12-05 00:21:07 +0100

    added app

---
+++ aesir-store/src/App.jsx
@@ -0,0 +1,13 @@
+import router from './router/router.jsx';
+import { RouterProvider } from 'react-router';
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
