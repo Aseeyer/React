1ba94a55045b78eeabedde1363f1604295bec2d9
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-12-26 22:53:33 +0100

    added productApi

---
+++ signUpAndLogin/src/features/products/productApi.jsx
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const productApi = createApi({
+  reducerPath: "productApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://dummyjson.com/",
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => "products",
+    }),
+
+    getProductById: builder.query({
+      query: (id) => `products/${id}`,
+    }),
+  }),
+});
+
+export const {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+} = productApi;
