3c2df2ee4e64d41f67b4d57bcf72306e77dec6d4
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-12-26 23:51:42 +0100

    updated the main by removing router browser due to a double router error

--- signUpAndLogin/src/main.jsx
+++ signUpAndLogin/src/main.jsx
@@ -7,8 +7,6 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </Provider>
 );
